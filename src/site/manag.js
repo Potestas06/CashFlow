bd20312cb7661c68a68af90ca6b6c5e58e2c16d8
Potestas06 <94400853+Potestas06@users.noreply.github.com>
2024-06-04 07:41:17 +0200

    MORE

--- src/site/manag.js
+++ src/site/manag.js
@@ -1,9 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
+import { db } from "../firebase";
 
 const Manage = () => {
   const navigate = useNavigate();
+  const [userDatas, setUserDatas] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection(auth.currentUser.uid)
+      .onSnapshot((snapshot) => {
+        setUserDatas(
+          snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        );
+      });
+    console.log(userDatas);
+    return unsubscribe;
+  }, []);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
