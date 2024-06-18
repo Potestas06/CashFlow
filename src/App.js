475c9ad5b3dc121fbd1e56581e4c89d0d92a3648
Potestas06 <94400853+Potestas06@users.noreply.github.com>
2024-06-18 08:09:59 +0200

    IAAAANNNN!!!!

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFound from "./site/NotFound";
 import Nav from "./Component/nav";
 import Home from "./site/home";
-import Dashboard from "./site/dashboard";
 import Foot from "./Component/fooooooot";
 import Login from "./site/login";
 import Register from "./site/register";
 import Manage from "./site/manag";
-import {useEffect, useState} from "react";
-import {auth} from "./firebase";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 function App() {
-    let [user, setUser] = useState(null);
+  let [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-            setUser(auth.currentUser);
-        })
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      setUser(auth.currentUser);
     });
+  });
   return (
     <Router>
       <header>
@@ -30,8 +29,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/manage" element={<Manage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
       <footer>
